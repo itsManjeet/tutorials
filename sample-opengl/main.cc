af441090596f36f0457d8dd9800b53b8fe023692
itsmanjeet <itsmanjeet1998@gmail.com>
2021-08-31 15:38:19 +0530

    added shader

--- sample-opengl/main.cc
+++ sample-opengl/main.cc
@@ -4,6 +4,55 @@ using namespace std;
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+/**
+ * 
+ **/
+unsigned int
+create_shader(string const &vertex_shader,
+              string const &fragment_shader)
+{
+
+    auto compile_shader = [](string const &source, uint type) -> uint
+    {
+        uint id = glCreateShader(type);
+        char const *src = source.c_str();
+        glShaderSource(id, 1, &src, nullptr);
+        glCompileShader(id);
+
+        int result;
+        glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+        if (!result)
+        {
+            int len;
+            glGetShaderiv(id, GL_INFO_LOG_LENGTH, &len);
+            char *mesg = new char[len];
+            glGetShaderInfoLog(id, len, &len, mesg);
+
+            cout << "Error! Failed to compile" << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " failed" << endl;
+            cout << mesg << endl;
+
+            glDeleteShader(id);
+            return 0;
+        }
+
+        return id;
+    };
+
+    auto program = glCreateProgram();
+    auto v_shader_id = compile_shader(vertex_shader, GL_VERTEX_SHADER);
+    auto f_shader_id = compile_shader(fragment_shader, GL_FRAGMENT_SHADER);
+
+    glAttachShader(program, v_shader_id);
+    glAttachShader(program, f_shader_id);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(v_shader_id);
+    glDeleteShader(f_shader_id);
+
+    return program;
+}
+
 int main(int ac, char **av)
 {
     GLFWwindow *window;
@@ -66,6 +115,23 @@ int main(int ac, char **av)
                           sizeof(float) * 2, // Space between starting and next vertex index
                           0);
 
+    string vertex_shared =
+        "#version 330 core\n"                      // Version of opengl
+        "layout(location = 0) in vec4 position;\n" // use starting index 0 of vertex as a Vector4 and store in "position" variable
+        "void main() {\n"
+        "  gl_Position = position;\n" // set gl_Position i.e. vertex
+        "}\n";
+
+    string fragment_shared =
+        "#version 330 core\n"
+        "layout(location = 0) out vec4 color;\n"
+        "void main() {\n"
+        "   color = vec4(1.0, 1.0, 0.0, 1.0);\n" // set color as RED = 1.0, GREEN = 1.0, BLUE = 0.0, ALPHA = 1.0
+        "}\n";
+
+    auto shader = create_shader(vertex_shared, fragment_shared);
+    glUseProgram(shader);
+
     while (!glfwWindowShouldClose(window))
     {
 
@@ -78,9 +144,8 @@ int main(int ac, char **av)
         /** 
          * SINGLE_BUFFER - Flush()
          * 
-         * DUBLE_BUFFER - SwapBuffer 
+         * DOUBLE_BUFFER - SwapBuffer 
          */
-
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
