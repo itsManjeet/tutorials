cc819f4d7f8a502ecbeb966779ca43e9bf350e24
itsmanjeet <itsmanjeet1998@gmail.com>
2021-08-31 12:05:08 +0530

    Colored Triangle

--- sample-opengl/main.cc
+++ sample-opengl/main.cc
@@ -5,16 +5,40 @@ using namespace std;
 
 void displayFunc()
 {
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    glColor3f(1.0, 1.0, 1.0);
+
+    glBegin(GL_TRIANGLES);
+    /**
+     *                    (0.0, 1.0) (GREEN)
+     * 
+     * 
+     *  (-1.0, 0.0) (RED)               (1.0, 0.0) (BLUE)
+     */
+
+        glColor3f(1, 0, 0); // RED
+        glVertex2f(-1.0, 0.0);
+
+        glColor3f(0, 1, 0); // GREEN
+        glVertex2f(0.0, 1.0);
+
+        glColor3f(0, 0, 1); // BLUE
+        glVertex2f(1.0, 0.0);
+
+    glEnd();
+
+    glFlush();
 }
+
 int main(int ac, char **av)
 {
     glutInit(&ac, av);
-    glutInitDisplayMode(GLUT_RGB);
+    glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
     glutInitWindowSize(400, 500);
     int window = glutCreateWindow("Sample");
 
     glClearColor(0.0, 0.0, 0.0, 0.0);
-    gluOrtho2D(0, 400, 500, 0);
     glutDisplayFunc(displayFunc);
 
     glutMainLoop();
